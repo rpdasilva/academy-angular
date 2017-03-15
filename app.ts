344e7e968f309ed1e94a40d1c05211227db5ab2e
Greg Wilson <greg.wilson@rangle.io>
2017-03-15 15:03:17 -0400

    Blocking out display of courses

--- app.ts
+++ app.ts
@@ -6,10 +6,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 // Root component of entire page.
 @Component({
     selector: 'app-component',
-    styles: [],
-    template: '<p>Application running</p>'
+    template: `
+<div id="courses">
+  <h2>{{courses.length}} Courses</h2>
+  <app-course *ngFor="let c of courses">
+    <p>course_ident: {{c.course_ident}}</p>
+    <p>course_name: {{c.course_name}}</p>
+  </app-course>
+</div>
+    `
 })
 export class AppComponent {
+
+    // Test data.
+    courses: Object[] = [
+	{"course_ident": 1,
+	 "course_name": "Bugs"},
+	{"course_ident": 2,
+	 "course_name": "Confusion"}
+    ];
 }
 
 // Boilerplate to get application running.
