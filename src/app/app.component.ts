150c61bf60abe60064a0234e25ebbec4b06c0780
Greg Wilson <greg.wilson@rangle.io>
2017-03-15 19:12:55 -0400

    Displaying faked data for the first time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Root component of entire page.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  // Test data.
+  courses: Object[] = [
+    {"course_ident": 1,
+     "course_name": "Bugs"},
+    {"course_ident": 2,
+     "course_name": "Confusion"}
+  ];
 }
