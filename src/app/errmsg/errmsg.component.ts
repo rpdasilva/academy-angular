81a64ec6614a5b29865b0e40ab6ed355ec6baa00
Greg Wilson <greg.wilson@rangle.io>
2017-03-26 14:06:15 -0400

    Using conditional styling

--- src/app/errmsg/errmsg.component.ts
+++ src/app/errmsg/errmsg.component.ts
@@ -17,12 +17,7 @@ export class ErrmsgComponent implements OnInit {
 
   ngOnInit() {
     this.store.errorMessage.subscribe(msg => {
-      if (msg) {
-	this.message = msg;
-      }
-      else {
-	this.message = '---';
-      }
+      this.message = msg;
     });
   }
 
